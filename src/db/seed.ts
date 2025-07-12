b63f0a14ce4db9159312c23b33b1b5d0ed0151a6
Tarcisio Maciel <tarcisio.maciel@gmgroupbrasil.com.br>
2025-07-12 13:22:24 -0300

    feat: add create question, create room and get room questions

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -7,10 +7,14 @@ await reset(db, schema)
 await seed(db, schema).refine((f) => {
   return {
     rooms: {
+      count: 20,
       columns: {
         name: f.companyName(),
         description: f.loremIpsum(),
       },
+    },
+    questions: {
+      count: 20,
     },
   }
 })
